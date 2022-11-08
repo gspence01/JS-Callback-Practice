fe211a310f4f7650e8ad927905806a5bc798041b
Gabrielle <gabrielle01.spence@gmail.com>
2022-11-07 16:38:47 -0800

    Created function to move character with arrow keys

--- index.js
+++ index.js
@@ -2,7 +2,11 @@ const inventory = newInventory()
 move(inventory).to(0, 0)
 
 const character = newImage('assets/green-character/static.gif')
-move(character).to(100, 250)
+let direction = null;
+let x= 100;
+let y=250;
+
+move(character).withArrowKeys(x, y)
 
 
 move(newImage('assets/tree.png')).to(200, 450)
